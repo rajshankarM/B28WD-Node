a497171aab5d7e13884f3a850c6772034b272781
rajshankarM <rajshankar607@gmail.com>
2021-12-01 12:11:11 +0530

    delete put and env

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // const express = require('express')
 import express from "express";
 import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const app = express()
 
@@ -9,9 +11,11 @@ app.use(express.json())
  
 const PORT = 9000;
 
-// const movies =;
+// const movies = ;
 
-const MONGO_URL = "mongodb://localhost";
+// const MONGO_URL = "mongodb://localhost";
+const MONGO_URL = process.env.MONGO_URL;
+// mongodb+srv://rajshankar:<password>@cluster0.xc4ii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 async function createConnection(){
 	const client = new MongoClient(MONGO_URL)
@@ -33,21 +37,7 @@ app.get("/movies",async (request, response) => {
 	if(filter.rating){
 		filter.rating = parseInt(filter.rating)
 	}
-    // console.log(language, rating);
-
-//    let filterMovies = movies;
-
-//    if(language){
-//      filterMovies = filterMovies.filter((mv) => mv.language == language);
-//    }
-
-//    if(rating){
-//     filterMovies = filterMovies.filter((mv) => mv.rating == +rating);
-//   }
-   const filterMovies = await client
-   .db("b28wd")
-   .collection("movies")
-   .find(filter).toArray();
+   const filterMovies = await getMovies(filter);
 
    response.send(filterMovies)
 })
@@ -55,10 +45,7 @@ app.get("/movies",async (request, response) => {
 app.post('/movies', async (request, response) => {
 	const data = request.body;
 	// console.log(data);
-	const result = await client
-	.db("b28wd")
-	.collection("movies")
-	.insertMany(data)
+	const result = await createMovies(data)
 	response.send(data);
 })
 
@@ -66,10 +53,7 @@ app.get("/movies/:id",async (request, response) => {
     console.log(request.params)
     const {id } = request.params
 
-	const movie = await client
-	.db("b28wd")
-	.collection("movies")
-	.findOne({ id: id })
+	const movie = await getMovieById(id)
     // const movie = movies.find((mv)=> mv.id == id)
     console.log(movie);
    
@@ -77,5 +61,63 @@ app.get("/movies/:id",async (request, response) => {
     movie ? response.send(movie): response.status(404).send ({msg: "No matching movie found"}) ;
 })
 
+app.delete("/movies/:id",async (request, response) => {
+    console.log(request.params)
+    const {id } = request.params
+
+	const result = await deleteMovieById(id)
+
+    result.deletedCount > 0
+	? response.send(result): response.status(404).send ({msg: "No matching movie found"}) ;
+});
+
+app.put("/movies/:id",async (request, response) => {
+    console.log(request.params)
+    const {id } = request.params
+	const data = request.body;
+
+	const result = await updateMovieById(id, data)
+	const movie = await getMovieById(id)
+	response.send(movie)
+
+});
+
+
 app.listen(PORT, () => console.log("App is start in", PORT))
  
+
+async function updateMovieById(id, data) {
+	return await client
+		.db("b28wd")
+		.collection("movies")
+		.updateOne({ id: id }, { $set: data });
+}
+
+async function createMovies(data) {
+	return await client
+		.db("b28wd")
+		.collection("movies")
+		.insertMany(data);
+}
+
+async function getMovies(filter) {
+	return await client
+		.db("b28wd")
+		.collection("movies")
+		.find(filter).toArray();
+}
+
+async function deleteMovieById(id) {
+	return await client
+		.db("b28wd")
+		.collection("movies")
+		.deleteOne({ id: id });
+}
+
+async function getMovieById(id) {
+	return await client
+		.db("b28wd")
+		.collection("movies")
+		.findOne({ id: id });
+}
+
